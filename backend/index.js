48ea867e2a153e24d1509312293428002afda3c7
Bruno Barbagelata <bruno140685@gmail.com>
2021-10-16 10:56:11 -0400

    setting backend

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const http = require("http");
-const mongoose = require("mongoose");
-const cors = require("cors");
+console.log("something");
+
 const app = express();
 
-const URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
+app.get("/search", (req, res) => {
+  console.log(req.query);
+  res.json();
+});
 
-mongoose
-  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((x) => console.log(`Connected to ${x.connections[0].name}`))
-  .catch(() => console.error("Error connecting to Mongo"));
-
-app.use(cors()); //for CORS
-app.use(express.json()); //for req.body
-app.use(`/api`, require("./routes")); //Let's us put all our routes in one seperate file and adds /api infront of i t
-
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`Port listening on 5000`)
-);
+app.listen(process.env.PORT || 5000);
