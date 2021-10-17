31a103a6300d05fa2989807f284a3d7d7069446b
Bruno Barbagelata <bruno140685@gmail.com>
2021-10-17 10:28:22 -0400

    mongoose added

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const http = require("http");
-console.log("something");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.get("/search", (req, res) => {
+  const resp = req.query;
   console.log(req.query);
-  res.json();
+  res.json(resp);
 });
 
 app.listen(process.env.PORT || 5000);
