00d051f07053287639c7a30b5b8488a80e7af3b8
Bruno Barbagelata <bruno140685@gmail.com>
2021-10-17 11:14:23 -0400

    Example.js model

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
+const Example = require("./models/Example");
+
+mongoose
+  .connect("mongodb://localhost:27017/exampleDB")
+  .then((self) =>
+    console.log(`Connected to the database: "${self.connection.name}"`)
+  );
 
 const app = express();
 
-app.get("/search", (req, res) => {
-  const resp = req.query;
-  console.log(req.query);
-  res.json(resp);
+app.post("/search", async (req, res) => {
+  const newExample = await Example.create(req.query);
+  console.log(newExample);
+  res.json(newExample);
 });
 
 app.listen(process.env.PORT || 5000);
